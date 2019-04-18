e3da66f36b27ad62febc0de2c6b38afa5e7ced43
Bhanu <bhanu.sada@gmail.com>
2019-04-18 17:49:30 -0400

    Looping

--- src/Components/Fields/Fields.js
+++ src/Components/Fields/Fields.js
@@ -16,12 +16,12 @@ const fields = (props) => {
         <small>
             <ul>
                 <li style={{backgroundColor: "rgba(0, 0, 0, 0.03)"}}>
-                    <span style={{width: "50%"}}>FIELD NAME</span>
-                    <span style={{width: "15%"}}>REQUIRED</span>
-                    <span style={{width: "15%"}}>DATATYPE</span>
-                    <span style={{width: "15%"}}>MIN/MAX</span>
-                    <span style={{width: "20%"}}>MANDATORY TEST</span>
-                    <span style={{width: "20%"}}>CONDITIONAL TEST</span>
+                    <span style={{width: "50%", fontWeight: 600}}>FIELD NAME</span>
+                    <span style={{width: "15%", fontWeight: 600}}>REQUIRED</span>
+                    <span style={{width: "15%", fontWeight: 600}}>DATATYPE</span>
+                    <span style={{width: "15%", fontWeight: 600}}>MIN/MAX</span>
+                    <span style={{width: "20%", fontWeight: 600}}>MANDATORY TEST</span>
+                    <span style={{width: "20%", fontWeight: 600}}>CONDITIONAL TEST</span>
                 </li>
                 {fieldslist}
             </ul>
